a203c8a9a186c9486f3a3f0b0726681fb0f63f91
MYAGAH\M.Zangane <mohsen.1398.pg@gmail.com>
2020-08-25 16:25:51 +0430

    Create Login feature and set username and password

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ let app=express();
 app.get("/",(req,res)=>{
     res.send("Hello World")
 })
+
+app.get("/login",(req,res)=>{
+    let username=req.body.username;
+    let password=req.body.password;
+    if(username&&password)
+    res.send({username:username,password:password})
+})
+
 app.listen("3800",()=>{
     console.log("Connect To Server.....")
 })
